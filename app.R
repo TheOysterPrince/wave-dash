12eba9eceabc1e6c59905b42d6c3279274570add
Eike <98490042+TheOysterPrince@users.noreply.github.com>
2025-08-03 18:42:52 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -9,6 +9,7 @@ packages <- c(
   "tidyverse",           # summary of basic r packages
   "lubridate",           # date and time manipulation
   "data.table",          # efficient large table handling
+  "arrow",               # reading and writing parquet files
   
   # plot packages
   "plotly",              # 3d plot
@@ -95,7 +96,30 @@ degrees_to_direction <- function(degree) {
 
 # ------ ml-model training -----------------------------------------------------
 # load task and trained model  
-load("model_and_task.RData")
+# read copernicus file of all stations
+model_data <- as.data.table(read_parquet("wave_cop.parquet"))
+
+# final training data
+final_data <- model_data %>% 
+                  select(lat, lon, time, station, VHM0, VCMX, VHM0_SW1, VTM01_WW, VTM02)
+
+# defining benchmark task
+final_tsk <- as_task_regr_st(final_data,
+                           id = "final", target = "VHM0",
+                           coordinate_names = c("lon", "lat"), coords_as_features = FALSE,
+                           crs = "4326")
+
+# setting role for time/ date variable 
+final_tsk$set_col_roles("time", roles = "time")
+final_tsk$set_col_roles(c("lon","lat"), roles = "space")
+final_tsk$set_col_roles("station", roles = "name")
+
+# define learner
+xgboost = lrn("regr.xgboost")
+
+# train model xgboost
+future::plan("multisession")
+xgboost$train(final_tsk)
 
 # ==== Dashboard: ==============================================================
 # ------ ui section ------------------------------------------------------------
